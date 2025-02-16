49e69a5471379951ce9801cf2950dbbd0df93641
anchan31 <151496115+anchan31@users.noreply.github.com>
2025-02-16 23:43:42 +0530

    Update script.js

--- script.js
+++ script.js
@@ -31,3 +31,12 @@
     window.location.href = productPageUrl;
   }
   
+  function toggleMenu() {
+    const navMenu = document.getElementById('nav-menu');
+    if (navMenu.style.display === 'flex') {
+      navMenu.style.display = 'none';
+    } else {
+      navMenu.style.display = 'flex';
+    }
+  }
+  
