bcd5a380b86f621f7006a8811d6ae2a0aff9b0da
anchan31 <151496115+anchan31@users.noreply.github.com>
2025-02-16 21:25:57 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,33 @@
+  function filterProducts() {
+    const filterValue = document.getElementById("product-filter").value;
+    const products = document.querySelectorAll(".product-card");
+  
+    products.forEach((product) => {
+      if (filterValue === "all" || product.getAttribute("data-category") === filterValue) {
+        product.style.display = "block"; // Show product
+      } else {
+        product.style.display = "none"; // Hide product
+      }
+    });
+  }
+  
+
+  function searchProducts() {
+    const query = document.getElementById("product-search").value.toLowerCase();
+    const products = document.querySelectorAll(".product-card");
+  
+    products.forEach((product) => {
+      // Combine text content from product title and description
+      const productText = product.textContent.toLowerCase();
+      if (productText.includes(query)) {
+        product.style.display = "block";
+      } else {
+        product.style.display = "none";
+      }
+    });
+  }
+  
+  function redirectToProductPage(productPageUrl) {
+    window.location.href = productPageUrl;
+  }
+  
